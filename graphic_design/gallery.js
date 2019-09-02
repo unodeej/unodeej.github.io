2bdfb977d0c72248b3527dcb56be1474e0fca476
DJ <unodeej@gmail.com>
2019-09-02 15:34:33 -0700

    Update gallery.js

--- graphic_design/gallery.js
+++ graphic_design/gallery.js
@@ -1,9 +1,9 @@
-let folder = "graphic_design/img/comics"; //TODO: change this to the path to your folder with the images.
-    $.ajax({
-        url: folder,
-        success: function(data) {
-            $(data).find("a").attr("href", function(i, val) {
-                $("body").append("<img src='" + folder + '/' + val + "'>");
-            });
-        }
-    });
+let folder = "https://github.com/unodeej/unodeej.github.io/tree/master/graphic_design/img/comics"; //TODO: change this to the path to your folder with the images.
+$.ajax({
+	url: folder,
+	success: function(data) {
+		$(data).find("a").attr("href", function(i, val) {
+			$("body").append("<img src='" + folder + '/' + val + "'>");
+		});
+	}
+});
