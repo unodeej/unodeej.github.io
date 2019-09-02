392b35b51513c69f0417455619b9f113d1791730
DJ <unodeej@gmail.com>
2019-09-02 14:52:08 -0700

    Update gallery.js

--- graphic_design/gallery.js
+++ graphic_design/gallery.js
@@ -1,4 +1,4 @@
-let folder = "test_images"; //TODO: change this to the path to your folder with the images.
+let folder = "https://github.com/unodeej/unodeej.github.io/tree/master/graphic_design/img/comics"; //TODO: change this to the path to your folder with the images.
     $.ajax({
         url: folder,
         success: function(data) {
