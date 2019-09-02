89cb14b099338fc1464bdecc0b95f53177ed1a33
DJ <unodeej@gmail.com>
2019-09-02 12:39:37 -0700

    Update Shooter.js

--- web_projects/pic_40a/Shooter/Shooter.js
+++ web_projects/pic_40a/Shooter/Shooter.js
@@ -147,7 +147,7 @@ function InstantiateAlien(y) {
 	alienID++;
 }
 
-document.addEventListener("keypress", 
+document.addEventListener("keydown", 
 	function(event) {
 		if (event.key === "d" || event.key === "D")
 		{
