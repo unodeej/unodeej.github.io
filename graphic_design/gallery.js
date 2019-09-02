680fb1ad2fb7d05220ad9cb1b3f466b94330ed97
DJ <unodeej@gmail.com>
2019-09-02 12:35:05 -0700

    images

---
+++ graphic_design/gallery.js
@@ -0,0 +1,9 @@
+var folder = "img/comics"; //TODO: change this to the path to your folder with the images.
+    $.ajax({
+        url: folder,
+        success: function(data) {
+            $(data).find("a").attr("href", function(i, val) {
+                $("body").append("<img src='" + folder + '/' + val + "'>");
+            });
+        }
+    });
