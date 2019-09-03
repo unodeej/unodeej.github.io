9f81452b2eb0c13016cb77a0fa1eea902bba728d
DJ <unodeej@gmail.com>
2019-09-03 08:52:06 -0700

    ch

--- graphic_design/gallery.js
+++
@@ -1,9 +0,0 @@
-let folder = "https://github.com/unodeej/unodeej.github.io/tree/master/graphic_design/img/comics"; //TODO: change this to the path to your folder with the images.
-$.ajax({
-	url: folder,
-	success: function(data) {
-		$(data).find("a").attr("href", function(i, val) {
-			$("body").append("<img src='" + folder + '/' + val + "'>");
-		});
-	}
-});
